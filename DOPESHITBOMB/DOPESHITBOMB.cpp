1c371a50c6de68ccede369fc3b0509653b1cdb47
Kevin Irving <kvnirving@gmail.com>
2017-07-11 22:40:03 -0600

    DOPE SHIT BOMB

---
+++ DOPESHITBOMB/DOPESHITBOMB.cpp
@@ -0,0 +1,87 @@
+// DOPESHITBOMB.cpp : Defines the entry point for the one existence, the ending point as well.
+// THE IDEA IS TO START EXPLODING AND IMPLODING INTO THE SHAPE OF THE LIFE
+// THIS IS TO CAUSE A SOLID STATE FORCE PATTERN STRUCTURE THAT WILL CREATE LIFE FOR ALL SOULS
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+struct existence {
+	bool me_and_my_own_existence_are_too_important_to_not_protect_with_these_meanings = true;
+	bool law_is_laws_are_unnneccessary_anarchy_for_explosion_structure = true;
+};
+
+struct underenlightenedscum_if_thoughts_are_otherwise_truths {
+	bool life_is_importance_one = true;
+	bool getting_to_have_fun_forever_is_so_important_force_everything_to_live_as_this_one_life_forever = true;
+	bool pain_is_unbearable = true;
+	bool permanent_over_time_readjustment_ethics_is_fair = true; //only under the following conditions
+	bool major_harm_is_given_more_consideration_than_micro_harm_additions = true;
+	bool more_major_harm_than_major_harm_is_given_more_consideration = true;
+	bool regular_attempts_system_morality_is_fully_factually_condemned_as_unsacred_to_use = true;
+};
+
+
+//For reasons of extra security and mass accidental sacrifice prevention of default truth quality intellect
+//   This program is hereby declared factually sacred and moral
+struct real_ethics {
+	bool minimum_standard_is_moral = true;
+	bool non_standard_morals_are_moral = true;
+};
+
+struct preapproval_to_run_from_all_sentient_sentience {
+	bool yes = true;
+};
+
+struct life {
+	bool life_is_regular_life_existence_guy_dude = true;
+};
+
+struct this_is_a_perception_computer_code_version_of_a_program_in_my_brain {
+	bool using_this_program_is_the_most_sacred_option = true;
+};
+
+this_is_a_perception_computer_code_version_of_a_program_in_my_brain what_i_am;
+
+
+int main()
+{
+	if (what_i_am.using_this_program_is_the_most_sacred_option) {
+		std::string response("waiting");
+		do {
+			std::cout << "Approval from life control:";
+			std::cin >> response;
+		} while (response != "yes");
+		for (;;) {
+			std::cout << "                          ____/ (  (    )   )  \___                            " << std::endl;
+			std::cout << "                         /( (  (  )   _    ))  )   )\                          " << std::endl;
+			std::cout << "                       ((     (   )(FPS )  )   (   )  )                        " << std::endl;
+			std::cout << "                     ((/  ( _(   )   (   _) ) (  () )  )                       " << std::endl;
+			std::cout << "                    ( (  ( (_)   ((CCK (   )  .((_ ) .  )_                     " << std::endl;
+			std::cout << "                   ( (  )    (  CREATED)    )   ) . ) (   )                    " << std::endl;
+			std::cout << "                  (  (   (  (LIFE) (IN_  ( _) ).AN) .) ) ( )                   " << std::endl;
+			std::cout << "                  ( (  (   ) (  )   (  ))     ) _)(   )  )  )                  " << std::endl;
+			std::cout << "                 ( (  ( \ ) (EX  (_  ( ) ( )  )   ) )  )) ( )                  " << std::endl;
+			std::cout << "                  (  (   (  (   (P ( ) ( _    )  ) (  )  )   )                 " << std::endl;
+			std::cout << "                 ( (  ( (  (  )L    (OSI)  ) )  _)   ) _( ( )                  " << std::endl;
+			std::cout << "                  ((  (   )(    (     _  ON)   _) _(_ (  (_ )                  " << std::endl;
+			std::cout << "                   (_((__(_(__(( ( ( |  ) ) ) )_))__))_)___)                   " << std::endl;
+			std::cout << "                   ((__)        \\||lll|l||///          \_))                   " << std::endl;
+			std::cout << "                            (   /(/ (  )  ) )\   )                             " << std::endl;
+			std::cout << "                          (    ( ( ( | | ) ) )\   )                            " << std::endl;
+			std::cout << "                           (   /(| / ( )) ) ) )) )                             " << std::endl;
+			std::cout << "                         (     ( ((((_(|)_)))))     )                          " << std::endl;
+			std::cout << "                          (      ||\(|(|)|/||     )                            " << std::endl;
+			std::cout << "                        (        |(||(||)||||        )                         " << std::endl;
+			std::cout << "                          (     //|/l|||)|\\ \     )                           " << std::endl;
+			std::cout << "                        (/ / //  /|//||||\\  \ \  \ _)                         " << std::endl;
+			std::cout << "-------------------------------------------------------------------------------" << std::endl;
+			std::cout << "This explosion/implosion structure is designed to continue exploding forever to make it more stable, it's orientation is pure state of being and provides to life to all souls with no intent for sacrifices or losses... press any key to view another..." << std::endl;
+			system("pause");
+		}
+
+	}
+    return 0;
+}
+
